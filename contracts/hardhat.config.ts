b16e57d3e5bf2aa477973cc5cd0f46cdca8d7259
DeborahOlaboye <deboraholaboye@gmail.com>
2025-11-13 11:48:28 +0100

    contracts deployed

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,41 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@typechain/hardhat";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    somniaTestnet: {
+      url: process.env.SOMNIA_RPC_URL || "https://dream-rpc.somnia.network",
+      chainId: 50312,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: "auto",
+    },
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
+  paths: {
+    sources: "./src",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
+export default config;
